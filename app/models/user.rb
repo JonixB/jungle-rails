9bf2fe6ced9abbfbaaf6a6cec83be0283aa7d54b
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-30 19:08:50 -0800

    add a new method authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,11 @@ class User < ApplicationRecord
     confirmation: true,
     length: { minimum: 8 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by_email(email)
+    user_password - user.authenticate(password)
+
+    user && user_password ? user : nil
 end
