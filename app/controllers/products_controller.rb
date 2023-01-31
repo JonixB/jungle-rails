4a898f78d2400c49c89e9f8efbee7f2b51a48cb8
Nikko Bansil <bjohnnikko@gmail.com>
2023-01-31 15:42:18 -0800

    remove basic http authentication

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,4 @@
 class ProductsController < ApplicationController
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USERNAME"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"]
-  
   def index
     @products = Product.all.order(created_at: :desc)
   end
